fbbbd0ac3d907ccebdc74422d49a270eb231ad0f
canw <c2097@live.com>
2017-09-28 12:16:05 +0800

    new name

--- app/src/main/java/com/canwdev/zephyr/util/Conf.java
+++ app/src/main/java/com/canwdev/zephyr/util/Conf.java
@@ -8,5 +8,5 @@ public class Conf {
     public static final String PREF_WEATHER_SAVE = "weatherCache";
 
     public static final String HEWEATHER_KEY = "74ac2716affa4279b5db3898be81c25d";
-    public static final String GITHUB_ADDRESS = "https://github.com/canwdev/learn_android_1stcode/tree/master/C1401-MyWeather";
+    public static final String GITHUB_ADDRESS = "https://github.com/canwdev/zephyr-weather";
 }
